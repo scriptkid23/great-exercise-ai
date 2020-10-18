166625e94a5e018672e00d83c210e9e07ee3790d
HoanDo <sieunhannhenkutesn@gmail.com>
2020-10-18 12:00:43 +0700

    update client

---
+++ build/precache-manifest.09fcac7fb2e3cae275f514ec50a9dcf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5592768730dd99b0389fee4470158e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91db8b32a06b301a06e2",
+    "url": "/static/css/main.4f060c06.chunk.css"
+  },
+  {
+    "revision": "925d59a173dba0317320",
+    "url": "/static/js/2.98078ad2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.98078ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91db8b32a06b301a06e2",
+    "url": "/static/js/main.8797fc17.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
